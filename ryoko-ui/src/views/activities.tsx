2383b572d3f566e56800ef4330fc78b39d911681
tatew <tatewilhelm@yahoo.com>
2025-04-05 14:54:12 -0400

    Add activities table

--- ryoko-ui/src/views/activities.tsx
+++ ryoko-ui/src/views/activities.tsx
@@ -1,7 +1,8 @@
-import { Heading, Text } from "@radix-ui/themes";
+import { Heading, Section, Table, Text } from "@radix-ui/themes";
 import React, { useEffect } from "react";
 import { RYOKO_API_SERVICE } from "../services/ryokoApiService";
 import { Activity } from "../interfaces/activity";
+import getSymbolFromCurrency from "currency-symbol-map";
 
 export const Activities: React.FC = () => {
     const [activities, setActivities] = React.useState<Activity[]>([]);
@@ -18,8 +19,43 @@ export const Activities: React.FC = () => {
 
     return (
         <React.Fragment>
-            <Heading size={"6"}>Activities</Heading>
-            <Text>Count of Activities: {activities.length}</Text>
+            <Section>
+                <Heading size={"6"}>Activities</Heading>
+            </Section>
+
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>Cost</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>Notes</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {activities.map((activity) => (
+                        <Table.Row key={activity.id}>
+                            <Table.Cell>
+                                <Text>{activity.name}</Text>
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Text>
+                                    {activity.cost > 0 ? `${getSymbolFromCurrency(activity.currency)}${activity.cost.toFixed(2)}` : "Free"}
+                                </Text>
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Text>{activity.notes || "No notes provided"}</Text>
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                    {activities.length === 0 && (
+                        <Table.Row>
+                            <Table.Cell colSpan={3}>
+                                <Text>No activities found.</Text>
+                            </Table.Cell>
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table.Root>
         </React.Fragment>
     );
 };
